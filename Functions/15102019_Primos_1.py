ff7f8bc25b15ce0b6ed272b50dfa3c9994f4cd1f
flcoutos <fernandolcouto@gmail.com>
2019-10-15 19:17:47 -0300

    Numeros Primos ate 'n' (Crivo Eratostenes) from PythonPro

---
+++ Functions/15102019_Primos_1.py
@@ -0,0 +1,21 @@
+"""
+FROM: https://forum.python.pro.br/t/numeros-primos-fiz-em-python-mas-era-em-java-ou-groovy/921/2
+
+Implementação do Crivo de Eratóstenes
+
+https://pt.wikipedia.org/wiki/Crivo_de_Erat%C3%B3stenes
+"""
+
+
+def crivo(n):
+    primos = [2]
+    numeros = list(range(3, n + 1, 2))
+    while numeros:
+        ultimo_primo = primos[-1]
+        numeros = [i for i in numeros if i % ultimo_primo != 0]
+        if numeros:
+            primos.append(numeros[0])
+    return primos
+
+
+print(crivo(1000))
